ab69936f718c609c2702c7bbbea18dc415b14a4b
Alazar Bekele <152716794+AlazarBekele@users.noreply.github.com>
2024-08-27 02:47:35 +0300

    Add files via upload

---
+++ Script/Function.js
@@ -0,0 +1,25 @@
+
+    function ActivePopup () {
+
+        let onclickpopUp = document.getElementById ("ComponentPop").classList = "SecondPopClass"
+        // let back = document.getElementById ("BodyBack").style = ("opacity")
+        // let onclickpopUp = document.getElementById ("ComponentPop").classList.add = "SecondCompClass";
+
+            let Cancle = document.getElementById ("CancleBtn").classList = "CanclePopCalss"
+}
+
+        function CancleFunc () {
+
+            let Cancle = document.getElementById ("CancleBtn").classList = "CancleClass"
+            let onclickpopUp = document.getElementById ("ComponentPop").classList = "FirstCompClass"
+            
+        }
+
+        const PhoneImg = document.getElementsByClassName ("Phone");
+            function CallPhone () {
+                alert ('0715351962');
+            }
+            
+    function ContactMe () {
+        
+    }
